9b615b5dfbe90b972389bde42ceb8a1a135c1221
Koro0 <ledane92@gmail.com>
2022-10-25 20:25:06 +0200

    create for use Fetch

---
+++ src/components/PostsServices/index.js
@@ -0,0 +1,36 @@
+const config = new Headers({
+  Authorization: `Bearer ${localStorage.getItem('connect')}`,
+  Accept: 'application/json',
+  'Content-Type': 'multipart/form-data',
+});
+
+export class PostsService {
+  static getAllPosts() {
+    return fetch('http://localhost:3000/api/posts/', {
+      method: 'GET',
+      headers: config,
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  }
+
+  static getOnePost() {
+    const url = new URL(window.location.href);
+    const postId = url.searchParams.get('id');
+    return fetch('http://localhost:3000/api/posts/' + postId, {
+      method: 'GET',
+      headers: config,
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  }
+
+  static createPost() {
+    return fetch('http://localhost:3000/api/posts/', {
+      method: 'POST',
+      headers: config,
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  }
+}
