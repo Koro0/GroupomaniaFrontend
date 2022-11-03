ffe57dfc44ebe24c921c09c90c36306e843465fc
Koro0 <ledane92@gmail.com>
2022-11-03 16:37:01 +0100

    clean & add path

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Connexion from './pages/Connexion';
-import Header from './components/Header'
-import Error from './components/Error'
-import LogIn from './pages/Connexion/LogIn'
-import Register from './pages/Connexion/Register'
-
+import Header from './components/Header';
+import Error from './components/Error';
+import LogIn from './pages/Connexion/LogIn';
+import Register from './pages/Connexion/Register';
+import NewPost from './pages/NewPost';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/" element={<Home />}>
+          <Route path="new_post" element={<NewPost />}></Route>
+        </Route>
         <Route path="/connexion" element={<Connexion />}>
           <Route path="login" element={<LogIn />}></Route>
           <Route path="register" element={<Register />}></Route>
         </Route>
-        <Route path='/*' element={<Error />}>
-        </Route>
+        <Route path="/*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
   );
