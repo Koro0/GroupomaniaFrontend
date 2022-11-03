f4aeb1a7a713c6cc96b2f93544e36d573a646212
Koro0 <ledane92@gmail.com>
2022-11-03 16:40:26 +0100

    add reacte import

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
+//import Posts from '../Posts'
+
 const Home = () => {
     return (
         <div>
             <h1> Page d'accueil</h1>
+            {/*<Posts />*/}
         </div>
     )
 }
