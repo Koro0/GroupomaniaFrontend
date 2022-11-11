98991c5fd48184fc62eb060efc27162870d120bf
Koro0 <ledane92@gmail.com>
2022-11-11 01:32:26 +0100

    change temporary route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import Posts from './pages/Posts';
 import Connexion from './pages/Connexion';
 import Header from './components/Header';
 import Error from './components/Error';
 import NewPost from './pages/NewPost';
 import Footer from './components/Footer';
+import Post from './pages/Post';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/home" element={<Home />}>
-          <Route path="new_post" element={<NewPost />} />
-        </Route>
-        <Route path="/" element={<Connexion />}></Route>
-        <Route path="/*" element={<Error />}></Route>
+        <Route exact path="/home" element={<Posts />} />
+        <Route path="/home/new_post" element={<NewPost />} />
+        <Route path="/home/:id" element={<Post />} />
+        <Route path="/" element={<Connexion />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
