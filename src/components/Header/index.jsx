4337e59e156077fe755f3a7830d61ebcd4678a60
Koro0 <ledane92@gmail.com>
2022-11-11 01:31:21 +0100

    add code to navigate on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
+import styled from 'styled-components'
+import Logo from '../../assets/logo.png'
 
-function Logo({ alt, props }) {
-    return <img alt={alt} src={props} />
-}
+
+const ButtonDeconnect = styled.button`
+color: var(--primaire)
+`
 
 const Header = () => {
-
+    const handleLogOut = () => {
+        localStorage.clear()
+    }
     return (
         <div>
             <Link to='/home'>
-                <Logo alt='Groupomania' src={logo} />
+                <img id='logo' src={Logo} alt='logo'></img>
             </Link>
             <nav>
                 <ul className='nav'>
                     <li className='navList'>
+                        <Link to='/home'>Home</Link>
+                    </li>
+                    <li className='navList'>
                         <Link to='/home/new_post'>New post</Link>
                     </li>
                     <li className='navList'>
-                        <Link to='/'>Deconnect</Link>
+                        <Link to='/'>
+                            <ButtonDeconnect onClick={handleLogOut} >Deconnect</ButtonDeconnect>
+                        </Link>
                     </li>
                 </ul>
             </nav>
-        </div>
+        </div >
     )
 }
 
