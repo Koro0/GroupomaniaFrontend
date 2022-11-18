3d7e6326088ee1a15f40bc9ae3a531d1a5bca083
Koro0 <ledane92@gmail.com>
2022-11-18 13:06:25 +0100

    add code for show one post

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import CommentBox from '../../components/Comment'
 import { PostsServices } from '../../components/PostsServices'
+
+const OnePost = styled.div`
+`
 
 export default function Post() {
     const [Post, setPost] = useState()
@@ -10,7 +15,17 @@ export default function Post() {
 
     const showPost = Post && Post.map((data) => {
         return (
-            <div>data.title</div>
+            <OnePost>
+                <div className='onePost-header'>
+                    <img src={data.imageUrl} alt="Post" />
+                    <h1>{data.title}</h1>
+                    <p>{data.description}</p>
+                </div>
+                <div>
+                    <CommentBox />
+                </div>
+
+            </OnePost>
         )
     })
     return (
