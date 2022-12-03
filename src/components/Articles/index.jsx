6612a1596973558c880c00d1dbf8a695d56358bb
Koro0 <ledane92@gmail.com>
2022-12-03 01:52:36 +0100

    clean style

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -1,40 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { PostsServices } from '../../components/PostsServices';
-import styled from 'styled-components';
-import { BiLike } from 'react-icons/bi'
-import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import Heart from '../Heart'
 
-const NumberLiked = styled.span`
-color: var(--secondary)
-`
+
 
 export default function Articles() {
-    const config = new Headers({
-        Authorization: `Bearer ${localStorage.getItem('connect')}`,
-        Accept: 'application/json',
-        'Content-Type': 'multipart/form-data',
-    });
-    const reqLike = { "userId": localStorage.getItem("user"), "like": 1 }
+
 
     const [posts, setPosts] = useState([])
-    //const [formAction, setFormAction] = useState(false)
+
     useEffect(() => {
         PostsServices.getAllPosts().then((data) => setPosts(data))
-        //setFormAction(false)
     }, [])
 
+    //Pb de like
     const handleLike = (e) => {
-        fetch('http://localhost:3500/api/posts/' + e + '/like', {
-            method: 'POST',
-            headers: config,
-            body: reqLike
-        })
-            .then((res) => res.json())
-            .catch((err) => console.log('msg: Like ' + err));
-        //setFormAction(true)
+        e.preventDefault();
+        PostsServices.likePost();
     }
-    /* changement en BootStrap les  styles et balise pour un meilleur rendus */
+
     return (
         <div className='article'>
             {
@@ -50,9 +35,9 @@ export default function Articles() {
                                 </Card.Title>
                                 <Card.Text>{data.description}</Card.Text>
                                 <Card.Footer className='p-footer'>
-                                    <Button className='likeButton' icon="pi" onClick={handleLike(data._id)}> <BiLike /></Button>
+                                    <div className='likeButton' icon="pi" onClick={handleLike}> <Heart /> </div>
                                     <Card.Text>
-                                        <NumberLiked> {data.likes} </NumberLiked>  J'aimes
+                                        {data.likes}   J'aimes
                                     </Card.Text>
                                 </Card.Footer>
                             </Card.Body>
