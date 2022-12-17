25ff7f2bebe6c5b6ac19c6bdb31b95de1cd14304
Koro0 <ledane92@gmail.com>
2022-12-17 12:11:25 +0100

    add axios

--- src/components/PostsServices/index.js
+++ src/components/PostsServices/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const config = new Headers({
   Authorization: `Bearer ${localStorage.getItem('connect')}`,
   Accept: 'application/json',
@@ -5,6 +7,8 @@ const config = new Headers({
 });
 
 export class PostsServices {
+  static urlApi = 'http://localhost:3500/api/posts/';
+
   static async getAllPosts() {
     try {
       const res = await fetch('http://localhost:3500/api/posts/', {
@@ -31,16 +35,30 @@ export class PostsServices {
     }
   }
 
-  static async createPost() {
-    try {
-      const res = await fetch('http://localhost:3500/api/posts/', {
-        method: 'POST',
-        headers: config,
-      });
-      return await res.json();
-    } catch (err) {
-      return console.log(err);
-    }
+  static async postPost(data) {
+    return await axios.post(this.urlApi, data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('connect')}`,
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  }
+
+  static async getCommentsByIdPost(id) {
+    return await axios.get(this.urlApi + `${id}/comments`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('connect')}`,
+      },
+    });
+  }
+
+  static async postCommentsByIdPost(id, data) {
+    return await axios.post(this.urlApi + `${id}/comment`, data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('connect')}`,
+      },
+    });
   }
 
   static async postComments() {
