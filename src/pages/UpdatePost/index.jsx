0ccbc5d2aa986d9bc9859d63998c6436e901be5a
Koro0 <ledane92@gmail.com>
2022-12-17 12:14:35 +0100

    create Put Post page

--- src/pages/UpdatePost/index.jsx
+++ src/pages/UpdatePost/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { PostsServices } from '../../components/PostsServices'
 
-export default function uplaodPost() {
+export default function UplaodPost() {
+    const [previewData, setPreviewData] = useState('')
+    useEffect(() => {
+        PostsServices.getOnePost().then((data) => setPreviewData(data))
+    }, [])
     return (
-        <div>uplaodPost</div>
+        <div>
+            <h1>uplaodPost</h1>
+            {previewData &&
+                <form onSubmit="">
+
+                    <div id='formTitle'>
+                        <label>Title :</label>
+                        <input placeholder={previewData.title} />
+                    </div>
+                    <div id='formDescription'>
+                        <label>Description :</label>
+                        <input placeholder="Your content " />
+                    </div>
+                    <div>
+                        <input type="file" accept='image/png, image/jpeg, image/jpg' />
+                    </div>
+                    <button type='submit' >Poster</button>
+                </form>
+            }
+        </div>
+
     )
 }
