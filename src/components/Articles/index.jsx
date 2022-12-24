576314be0bfb7ae51e388537ae84ee4462aea28c
Koro0 <ledane92@gmail.com>
2022-12-24 16:00:22 +0100

    delete like button

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -1,24 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { PostsServices } from '../../components/PostsServices';
 import Card from 'react-bootstrap/Card'
-import Heart from '../Heart'
 
 
 
 export default function Articles() {
-
-
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
         PostsServices.getAllPosts().then((data) => setPosts(data))
     }, [])
-
-    //Pb de like
-    const handleLike = (e) => {
-        e.preventDefault();
-        PostsServices.likePost();
-    }
 
     return (
         <div className='article'>
@@ -34,12 +25,6 @@ export default function Articles() {
                                     </Card.Link>
                                 </Card.Title>
                                 <Card.Text>{data.description}</Card.Text>
-                                <Card.Footer className='p-footer'>
-                                    <div className='likeButton' icon="pi" onClick={handleLike}> <Heart /> </div>
-                                    <Card.Text>
-                                        {data.likes}   J'aimes
-                                    </Card.Text>
-                                </Card.Footer>
                             </Card.Body>
                             <Card.Footer>
                                 <p>{data.updateDate}</p>
