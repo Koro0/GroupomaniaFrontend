eb0a827e7a3be6669ab86f760600ba9bdeab6e2a
Koro0 <ledane92@gmail.com>
2022-12-29 00:14:02 +0100

    clean code

--- src/pages/NewPost/index.jsx
+++ src/pages/NewPost/index.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { PostsServices } from '../../components/PostsServices'
 import { useForm } from 'react-hook-form'
-import { useEffect } from 'react'
 
 
-const DescriptionWindows = styled.textarea`
+/*const DescriptionWindows = styled.textarea`
 display: block;
  width: 100%;
  padding: 0.375rem 0.75rem;
@@ -20,7 +19,7 @@ display: block;
  appearance: none;
  border-radius: 0.375rem;
  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
- `
+ `*/
 // const PostForm = styled.form`
 //      margin: 2em auto;
 //      max-width: 750px;
@@ -35,21 +34,16 @@ display: flex;
 
 
 const NewPost = () => {
-
     const { register, handleSubmit, formState: { errors } } = useForm()
-    const [data, setData] = useState("")
-    console.log('this is :' + data)
-    console.log('stringyify :' + JSON.stringify(data))
-    let formData = new FormData()
-    //console.log(formData)
-    formData.append('imageUrl', File)
-    useEffect(() => {
-        PostsServices.postPost(data)
-    }, [data])
-    const onSubmit = async (data) => {
-        setData(data);
-    }
 
+    const onSubmit = async (data) => {
+        const formData = new FormData()
+        formData.append('imageUrl', data.imageUrl[0])
+        formData.append('title', data.title)
+        formData.append('description', data.description)
+        PostsServices.newPost(formData);
+    }
+    //styles a refire
 
     return (
         <FormDiv>
@@ -57,17 +51,17 @@ const NewPost = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div id='formTitle'>
                     <label>Title :</label>
-                    <input {...register("title", { required: true, maxLength: 20 })} placeholder="Your post Title" />
+                    <input type="text"  {...register('title')} placeholder="Your post Title" />
                     {errors.title && <span >The title is required</span>}
                 </div>
                 <div id='formDescription'>
                     <label>Description :</label>
-                    <DescriptionWindows {...register('description')} placeholder="Your content " />
+                    <textarea  {...register('description')} placeholder="Your content " />
                     {errors.description && <span>the minimum is to required</span>}
                 </div>
                 <div>
-                    <input type="file" {...register("imageUrl", { required: true })} accept='image/png, image/jpeg, image/jpg' />
-                    {errors.description && <span>the picture is required</span>}
+                    <input {...register('imageUrl')} type="file" accept='image/png, image/jpeg, image/jpg' />
+                    {errors.description && <span className='errorFormNewPost'>the picture is required</span>}
                 </div>
                 <button type='submit' >Poster</button>
             </form>
