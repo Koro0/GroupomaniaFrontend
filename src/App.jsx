2ff2075d5ac848fc9ccbaae627115b6c0efc9329
Koro0 <ledane92@gmail.com>
2022-12-30 00:43:51 +0100

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Context from './components/Context';
+import { useEffect } from 'react';
 import './styles/App.scss';
+
 import Connexion from './pages/Connexion';
 import ConnexionLogIn from './pages/Connexion/LogIn';
 import ConnexionRegister from './pages/Connexion/Register';
@@ -13,20 +18,19 @@ import Footer from './components/Footer';
 import PostsPut from './pages/UpdatePost';
 import UserProfile from './pages/UserProfile';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
-import { useState } from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Context from './components/Context';
+import { UserService } from './components/UserServices';
 function App() {
   const [logged, setLogged] = useState(false);
-  const [user, setUser] = useState('');
-  const [token, setToken] = useState('');
-
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    UserService.checkAdmin().then((res) => setIsAdmin(res.data))
+  }, [])
   return (
     <BrowserRouter>
       <ThemeProvider
         breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
         minBreakpoint="xxs">
-        <Context.Provider value={{ logged, setLogged, user, setUser, token, setToken }}>
+        <Context.Provider value={{ logged, setLogged, isAdmin, setIsAdmin }}>
           <Header />
           <Routes>
             <Route exact path="/home" element={<Posts />} />
