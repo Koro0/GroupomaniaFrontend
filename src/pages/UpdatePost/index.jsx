4ce4ec1a03376873b6fa110a72f0a7aa3199c03f
Koro0 <ledane92@gmail.com>
2022-12-29 18:16:17 +0100

    get data to api and modify to put

--- src/pages/UpdatePost/index.jsx
+++ src/pages/UpdatePost/index.jsx
@@ -2,28 +2,42 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { PostsServices } from '../../components/PostsServices'
+import { useForm } from 'react-hook-form'
+
+
 
 export default function UplaodPost() {
-    const [previewData, setPreviewData] = useState('')
+    const { register, handleSubmit } = useForm()
+
+    const [post, setPost] = useState()
     useEffect(() => {
-        PostsServices.getOnePost().then((data) => setPreviewData(data))
+        PostsServices.getOnePost().then((res) => setPost(res))
     }, [])
+
+    const onSubmit = (data) => {
+        const formData = new FormData()
+        formData.append('imageUrl', data.imageUrl[0])
+        formData.append('title', data.title)
+        formData.append('description', data.description)
+        PostsServices.ModifyPost(formData)
+
+    }
+
     return (
         <div>
-            <h1>uplaodPost</h1>
-            {previewData &&
-                <form onSubmit="">
-
+            <h1>Edit Your Post</h1>
+            {post &&
+                <form className='modifyForm' onSubmit={handleSubmit(onSubmit)}>
                     <div id='formTitle'>
                         <label>Title :</label>
-                        <input placeholder={previewData.title} />
+                        <input defaultValue={post.title} {...register('title')} />
                     </div>
                     <div id='formDescription'>
                         <label>Description :</label>
-                        <input placeholder="Your content " />
+                        <textarea className='modifyTexteContent' {...register('description')} defaultValue={post.description} />
                     </div>
                     <div>
-                        <input type="file" accept='image/png, image/jpeg, image/jpg' />
+                        <input type="file" {...register('imageUrl')} accept='image/png, image/jpeg, image/jpg' />
                     </div>
                     <button type='submit' >Poster</button>
                 </form>
