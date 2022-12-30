722d9c793060da0ce30a82cd12208419b164d2d7
Koro0 <ledane92@gmail.com>
2022-12-31 00:34:04 +0100

    clean code

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,52 @@
 import React from 'react'
 import Logo from '../Logo/logo'
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+import { useContext } from 'react';
+import Context from "../Context";
 
 const Header = () => {
     const [isLogged, setIslogged] = useState(false)
+    const { logged } = useContext(Context)
     const handleLogOut = () => {
         localStorage.clear()
         setIslogged(false)
     }
+    const isConnected = () => {
+        if (isLogged || logged) {
+            return (<Nav.Link href="/" onClick={handleLogOut}>Deconnect</Nav.Link>)
+        } else { return null }
+    }
+
     useEffect(() => {
         (localStorage.getItem('connect') && setIslogged(true))
     }, [])
+
     return (
-        <div>
-            <Navbar className='navbar' variant="light">
-                <Container>
-                    <NavbarBrand className='logoSvg'>
-                        <Logo />
-                    </NavbarBrand>
-                    <Nav className="me-auto">
-                        <Nav.Link href={isLogged ? "/home" : "/"}>Home</Nav.Link>
-                        <Nav.Link href={isLogged ? "/add_post" : "/"}>Add Post</Nav.Link>
-                        {
-                            (isLogged && <Nav.Link href="/" onClick={handleLogOut}>Deconnect</Nav.Link>)
-                        }
-                    </Nav>
-                </Container>
+        <header>
+            <Navbar className='navbar-expand-md' >
+                <NavbarBrand className='logoSvg'>
+                    <Logo />
+                </NavbarBrand>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item active">
+                        <Nav.Link href={isLogged || logged ? "/home" : "#"}>Home</Nav.Link>
+                    </li>
+                    <li className='nav-item active'>
+                        <Nav.Link href={isLogged || logged ? "/add_post" : "#"}>Add Post</Nav.Link>
+                    </li>
+                    <li className='nav-item active'>
+                        {isConnected()}
+                    </li>
+                </ul>
             </Navbar>
-        </div >
+        </header >
     )
 }
 
