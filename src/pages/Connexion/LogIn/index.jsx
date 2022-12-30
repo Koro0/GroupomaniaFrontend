1851e58165b26c2253f8180f376cfc4da0221f48
Koro0 <ledane92@gmail.com>
2022-12-31 00:36:54 +0100

    change styles, add  async/await for fetch & change navigate

--- src/pages/Connexion/LogIn/index.jsx
+++ src/pages/Connexion/LogIn/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
@@ -7,20 +7,14 @@ import { classNames } from "primereact/utils";
 import { useNavigate } from 'react-router-dom';
 import { UserService } from "../../../components/UserServices";
 import { useContext } from "react";
-import LoggedContext from "../../../components/LoggedContext";
+import Context from "../../../components/Context";
 
 
 
 export default function LogIn() {
-    const { logged, setLogged } = useContext(LoggedContext)
+    const { setLogged } = useContext(Context)
     const navigate = useNavigate()
-    useEffect(() => {
-        if (logged) {
-            navigate('/home')
-        } else {
-            navigate('/')
-        }
-    }, [logged, navigate])
+
 
     const defaultValues = {
         email: "",
@@ -34,15 +28,14 @@ export default function LogIn() {
         reset
     } = useForm({ defaultValues });
 
-    const onSubmit = (data) => {
-        console.log(data)
-        UserService.logIn(data).then((res) => {
-            localStorage.setItem('connect', res.data.token)
-            localStorage.setItem('user', res.data.userId)
+    const onSubmit = async (data) => {
+        await UserService.logIn(data).then((res) => {
+            localStorage.setItem('connect', res.data.token);
+            localStorage.setItem('user', res.data.userId);
             setLogged(true);
-            console.log(res.data)
-        }).catch(() => setLogged(false))
+        })
         reset();
+        navigate('/home')
     };
 
     const getFormErrorMessage = (name) => {
@@ -52,71 +45,75 @@ export default function LogIn() {
     };
 
     return (
-        <div className="form-demo">
-            <div className="flex justify-content-center">
+        <div className="form-demo col-sm-5">
+            <div className="form-box">
                 <div className="card">
-                    <h5 className="text-center">LogIn</h5>
-                    <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
-                        <div className="field">
-                            <span className="p-float-label p-input-icon-right">
-                                <i className="pi pi-envelope" />
-                                <Controller
-                                    name="email"
-                                    control={control}
-                                    rules={{
-                                        required: "Email is required.",
-                                        pattern: {
-                                            value: /^[A-Z0-9._%+-]+@groupomania.[A-Z]{2,4}$/i,
-                                            message: "Invalid email address. E.g. example@groupomania.com"
-                                        }
-                                    }}
-                                    render={({ field, fieldState }) => (
-                                        <InputText
-                                            id={field.name}
-                                            {...field}
-                                            className={classNames({
-                                                "p-invalid": fieldState.invalid
-                                            })}
-                                        />
-                                    )}
-                                />
-                                <label
-                                    htmlFor="email"
-                                    className={classNames({ "p-error": !!errors.email })}
-                                >
-                                    Email
-                                </label>
-                            </span>
-                            {getFormErrorMessage("email")}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller
-                                    name="password"
-                                    control={control}
-                                    rules={{ required: "Password is required." }}
-                                    render={({ field, fieldState }) => (
-                                        <Password
-                                            id={field.name}
-                                            {...field}
-                                            toggleMask
-                                            className={classNames({
-                                                "p-invalid": fieldState.invalid
-                                            })}
-                                        />
-                                    )}
-                                />
-                                <label
-                                    htmlFor="password"
-                                    className={classNames({ "p-error": errors.password })}
-                                >
-                                    Password*
-                                </label>
-                            </span>
-                            {getFormErrorMessage("password")}
-                        </div>
-                        <Button type="submit" label="Login" className="mt-2" />
-                    </form>
+                    <div className="form-top">
+                        <h5 className="text-center">LogIn</h5>
+                    </div>
+                    <div className="form-bottom">
+                        <form onSubmit={handleSubmit(onSubmit)} className="p-fluid login-form">
+                            <div className="field">
+                                <span className="p-float-label p-input-icon-right">
+                                    <i className="pi pi-envelope" />
+                                    <Controller
+                                        name="email"
+                                        control={control}
+                                        rules={{
+                                            required: "Email is required.",
+                                            pattern: {
+                                                value: /^[A-Z0-9._%+-]+@groupomania.[A-Z]{2,4}$/i,
+                                                message: "Invalid email address. E.g. example@groupomania.com"
+                                            }
+                                        }}
+                                        render={({ field, fieldState }) => (
+                                            <InputText
+                                                id={field.name}
+                                                {...field}
+                                                className={classNames({
+                                                    "p-invalid": fieldState.invalid
+                                                })}
+                                            />
+                                        )}
+                                    />
+                                    <label
+                                        htmlFor="email"
+                                        className={classNames({ "p-error": !!errors.email })}
+                                    >
+                                        Email
+                                    </label>
+                                </span>
+                                {getFormErrorMessage("email")}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller
+                                        name="password"
+                                        control={control}
+                                        rules={{ required: "Password is required." }}
+                                        render={({ field, fieldState }) => (
+                                            <Password
+                                                id={field.name}
+                                                {...field}
+                                                toggleMask
+                                                className={classNames({
+                                                    "p-invalid": fieldState.invalid
+                                                })}
+                                            />
+                                        )}
+                                    />
+                                    <label
+                                        htmlFor="password"
+                                        className={classNames({ "p-error": errors.password })}
+                                    >
+                                        Password*
+                                    </label>
+                                </span>
+                                {getFormErrorMessage("password")}
+                            </div>
+                            <Button type="submit" label="Login" className="mt-2" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </div >
