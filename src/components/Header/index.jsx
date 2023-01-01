224345248e293ad2de235097d3e4d5ba4ccc733f
Koro0 <ledane92@gmail.com>
2023-01-01 23:26:46 +0100

    logo change to link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Logo from '../Logo/logo'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useContext } from 'react';
@@ -27,24 +26,26 @@ const Header = () => {
 
     return (
         <header>
-            <Navbar className='navbar-expand-md' >
-                <NavbarBrand className='logoSvg'>
+            <Navbar >
+                <Nav.Link href={isLogged || logged ? "/home" : "#"} className='logoSvg'>
                     <Logo />
-                </NavbarBrand>
+                </Nav.Link>
                 <button class="nav-toggle" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <ul className="navbar-nav me-auto">
-                    <li className="nav-item active">
-                        <Nav.Link href={isLogged || logged ? "/home" : "#"}>Home</Nav.Link>
-                    </li>
-                    <li className='nav-item active'>
-                        <Nav.Link href={isLogged || logged ? "/add_post" : "#"}>Add Post</Nav.Link>
-                    </li>
-                    <li className='nav-item active'>
-                        {isConnected()}
-                    </li>
-                </ul>
+                <div className='navUl'>
+                    <ul className="navbar-nav me-auto ">
+                        <li className="nav-item active">
+                            <Nav.Link href={isLogged || logged ? "/home" : "#"}>Home</Nav.Link>
+                        </li>
+                        <li className='nav-item active'>
+                            <Nav.Link href={isLogged || logged ? "/add_post" : "#"}>Add Post</Nav.Link>
+                        </li>
+                        <li className='nav-item active'>
+                            {isConnected()}
+                        </li>
+                    </ul>
+                </div>
             </Navbar>
         </header >
     )
