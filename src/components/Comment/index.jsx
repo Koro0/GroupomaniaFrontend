5bb1a56bfcf21459a1e8460fd230ede73012e409
Koro0 <ledane92@gmail.com>
2023-01-02 15:55:14 +0100

    add styles & reset input

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,17 +2,25 @@ import React, { useState } from 'react'
 import { useEffect, useCallback } from 'react'
 import { PostsServices } from '../../components/PostsServices'
 import { useForm } from "react-hook-form";
+import styled from 'styled-components'
+
+
+const ButtonSubmit = styled.input`
+width:100%;
+`
 
 export default function Comments({ idPost }) {
   const [comments, setComments] = useState([])
-  const { register, handleSubmit } = useForm();
+  if (comments.length > 0) {
+    comments.sort((a, b) => new Date(b.date) - new Date(a.date));
+  }
+  const { register, handleSubmit, reset } = useForm();
 
   const refreshComments = useCallback(() => {
     PostsServices.getCommentsByIdPost(idPost).then(res =>
       setComments(res.data)
     );
   }, [idPost])
-  ///if (comments > 0) { comments.sort((a, b) => new Date(b.date) - new Date(a.date)) }
 
   useEffect(() => {
     refreshComments();
@@ -20,7 +28,7 @@ export default function Comments({ idPost }) {
 
   const onSubmit = (data) => {
     PostsServices.postCommentsByIdPost(idPost, data).then(() => refreshComments())
-    console.log(idPost)
+    reset()
   };
 
   return (
@@ -29,12 +37,16 @@ export default function Comments({ idPost }) {
         <h4 class="card-title">Recent Comments</h4>
         <h6 class="card-subtitle">Latest Comments section by users</h6>
       </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <textarea id="commentaire" type="text" placeholder='Commentaire' {...register('commentaire', { required: true, minLength: 2 })} />
+        <ButtonSubmit type="submit" />
+      </form>
       <div class="comment-widgets m-b-20">
         {comments.map((comment, index) =>
           <div class="d-flex flex-row comment-row">
             <div class="comment-text w-100">
               <ul>
-                <li className='commentLi' key={index}>
+                <li className='commentLi' key={(index, comment.user)}>
                   <h5>{comment.user}</h5>
                   <div class="comment-footer">
                     <span class="date">{comment.date}</span>
@@ -46,11 +58,6 @@ export default function Comments({ idPost }) {
           </div>
         )}
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <textarea id="commentaire" type="text" placeholder='Commentaire' {...register('commentaire', { required: true, minLength: 2 })} />
-        <input type="submit" />
-      </form>
-
     </div>
   )
 }
