5db82e586269f9fd4cfb126f401f5f9c5b88e627
Koro0 <ledane92@gmail.com>
2023-01-02 15:23:39 +0100

    clean code

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -15,13 +15,16 @@ export default function Articles() {
             console.log(data)
         })
     }, [])
+
     return (
         <div className='article row'>
             {
                 posts && posts.length > 0 && posts.map((data) => {
                     return (
                         <Card className='cards col-md-4' key={data._id}>
-                            {data.imageUrl !== null && <Card.Img className='card-img-top' src={data.imageUrl} alt={data.title} />}
+                            {data.imageUrl !== null &&
+                                <img className='card-img-top' src={data.imageUrl} alt={data.title} />
+                            }
                             <Card.Body className='p-body'>
                                 <Card.Title>
                                     <Card.Link href={'/' + data._id}>
