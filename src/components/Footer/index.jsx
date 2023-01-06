1d94c4dcbd9cb2ef00163832b6d0923f7246cfa3
Koro0 <ledane92@gmail.com>
2023-01-06 01:10:10 +0100

    automate date

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Footer() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
     return (
         <div>
-            <p className='footerCopy'>Copyright © Groupomania 2022.</p>
+            <p className='footerCopy'>Copyright © Groupomania {currentYear}.</p>
         </div>
     )
 }
